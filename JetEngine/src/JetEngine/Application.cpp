bb8300b8a2a586d33cf7a09bd28084010538ddea
jetfire <flash-gerych@yandex.ru>
2023-04-09 16:20:09 +0300

    added input polling

--- JetEngine/src/JetEngine/Application.cpp
+++ JetEngine/src/JetEngine/Application.cpp
@@ -4,7 +4,7 @@
 
 #include "glad/glad.h"
 
-
+#include "Input.h"
 
 
 namespace JetEngine {
@@ -62,6 +62,9 @@ namespace JetEngine {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			JE_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
