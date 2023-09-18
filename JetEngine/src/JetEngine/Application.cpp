421f369ee6de7348ce796089f02b26edf2e5fee7
jetfire13 <flash-gerych@yandex.ru>
2023-09-18 13:57:52 +0300

    Added buffer layout descriptors for vertex buffers

--- JetEngine/src/JetEngine/Application.cpp
+++ JetEngine/src/JetEngine/Application.cpp
@@ -15,6 +15,27 @@ namespace JetEngine {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case JetEngine::ShaderDataType::Float:		return GL_FLOAT;
+			case JetEngine::ShaderDataType::Float2:		return GL_FLOAT;
+			case JetEngine::ShaderDataType::Float3:		return GL_FLOAT;
+			case JetEngine::ShaderDataType::Float4:		return GL_FLOAT;
+			case JetEngine::ShaderDataType::Mat3:		return 3 * 3;
+			case JetEngine::ShaderDataType::Mat4:		return 4 * 4;
+			case JetEngine::ShaderDataType::Int:		return GL_INT;
+			case JetEngine::ShaderDataType::Int2:		return GL_INT;
+			case JetEngine::ShaderDataType::Int3:		return GL_INT;
+			case JetEngine::ShaderDataType::Int4:		return GL_INT;
+			case JetEngine::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		JE_CORE_ASSERT(false, "Unknoiwn ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		JE_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -29,16 +50,37 @@ namespace JetEngine {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;		
+
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
 
 
 		uint32_t indices[3] = { 0, 1, 2 };
@@ -49,13 +91,16 @@ namespace JetEngine {
 		std::string vertexSrc = R"(
 			#version 330 core
 			
-			layout(location = 0) in vec3 a_Position;
-			
+			layout(location = 0) in vec3 a_Position;			
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}		
 		)";
@@ -66,10 +111,12 @@ namespace JetEngine {
 			layout(location = 0) out vec4 color;
 			
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}		
 		)";
 
