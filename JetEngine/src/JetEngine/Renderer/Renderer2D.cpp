2df4f68b88380d68064e4fe48ae41bed0c8a1be7
jetfire13 <flash-gerych@yandex.ru>
2023-12-24 11:30:34 +0300

    added draw call splitting and stats for Renderer2D

--- JetEngine/src/JetEngine/Renderer/Renderer2D.cpp
+++ JetEngine/src/JetEngine/Renderer/Renderer2D.cpp
@@ -22,9 +22,9 @@ namespace JetEngine {
 
 	struct Renderer2DData
 	{
-		const uint32_t MaxQuads = 10000;
-		const uint32_t MaxVertices = MaxQuads * 4;
-		const uint32_t MaxIndices = MaxQuads * 6;
+		static const uint32_t MaxQuads = 10000;
+		static const uint32_t MaxVertices = MaxQuads * 4;
+		static const uint32_t MaxIndices = MaxQuads * 6;
 		static const uint32_t MaxTextureSlots = 32; // TODO: RenderCaps
 
 		Ref<VertexArray> QuadVertexArray;
@@ -40,6 +40,9 @@ namespace JetEngine {
 		uint32_t TextureSlotsIndex = 1; // 0 = white texture;
 
 		glm::vec4 QuadVertexPositions[4];
+
+
+		Renderer2D::Statistics Stats;
 	};
 
 	static Renderer2DData s_Data;
@@ -144,6 +147,18 @@ namespace JetEngine {
 			s_Data.TextureSlots[i]->Bind(i);
 		}
 		RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+		
+		s_Data.Stats.DrawCalls++;
+	}
+
+	void Renderer2D::FlushEndReset()
+	{
+		EndScene();
+		s_Data.QuadIndexCount = 0;
+		s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+		s_Data.TextureSlotsIndex = 1;
+		
 	}
 
 	// Privitives
@@ -154,6 +169,9 @@ namespace JetEngine {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
 		JE_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushEndReset();
 
 		const float textureIndex = 0.0f; // White Texture
 		const float tilingFactor = 1.0f;
@@ -190,6 +208,8 @@ namespace JetEngine {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.Stats.QuadCount++;
 
 		/*s_Data.TextureShader->SetFloat("u_TilingFactor", 1.0f);
 		s_Data.WhiteTexture->Bind();
@@ -209,6 +229,9 @@ namespace JetEngine {
 	{
 		JE_PROFILE_FUNCTION();
 
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushEndReset();
+
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
 		float textureIndex = 0.0f;
@@ -261,6 +284,8 @@ namespace JetEngine {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
@@ -270,6 +295,9 @@ namespace JetEngine {
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
 		JE_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushEndReset();
 
 		const float textureIndex = 0.0f; // White Texture
 		const float tilingFactor = 1.0f;
@@ -307,14 +335,20 @@ namespace JetEngine {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.Stats.QuadCount++;
 	}
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
 	{
 		DrawRotatedQuad({ position.x, position.y, 0.0f }, size, rotation, texture, tilingFactor, tintColor);
 	}
+	
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
 	{
 		JE_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushEndReset();
 
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
@@ -369,6 +403,18 @@ namespace JetEngine {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.Stats.QuadCount++;
+	}
+
+	void Renderer2D::ResetStats()
+	{
+		memset(&s_Data.Stats, 0, sizeof(Statistics));
+	}
+
+	Renderer2D::Statistics Renderer2D::GetStats()
+	{
+		return s_Data.Stats;
 	}
 
 }
--- SandBox/src/Sandbox2D.cpp
+++ SandBox/src/Sandbox2D.cpp
@@ -30,6 +30,7 @@ void Sandbox2D::OnUpdate(JetEngine::Timestep ts)
 	m_CameraController.OnUpdate(ts);
 	
 	// Render
+	JetEngine::Renderer2D::ResetStats();
 	{
 		JE_PROFILE_SCOPE("Renderer Prep");
 		JetEngine::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -48,11 +49,23 @@ void Sandbox2D::OnUpdate(JetEngine::Timestep ts)
 		JetEngine::Renderer2D::DrawQuad({ 0.7f, -0.2f }, { 0.5f, 0.75f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		JetEngine::Renderer2D::DrawQuad({ -0.2f, 0.0f, 0.1f }, { 0.75f, 0.5f }, { 0.3f, 0.1f, 0.7f, 1.0f });
 
-		JetEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerpoardTexture, 5.0f, {0.2, 0.8, 0.5f, 1.0f});
-		JetEngine::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_LogoTexture, 1.0f, { 0.2, 0.8, 0.5f, 1.0f });
+		JetEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerpoardTexture, 5.0f, {0.2, 0.8, 0.5f, 1.0f});
+		//JetEngine::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_LogoTexture, 1.0f, { 0.2, 0.8, 0.5f, 1.0f });
 		JetEngine::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.1f }, { 2.5f, 2.5f }, roration, m_CheckerpoardTexture, 5.0f, { 0.5, 0.5, 0.0f, 1.0f });
 
 		JetEngine::Renderer2D::EndScene();
+		
+		JetEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.f, 0.4f, (y + 5.0f) / 10.f, 0.7f };
+				JetEngine::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
+		JetEngine::Renderer2D::EndScene();
+	
 	}
 }
 
@@ -60,6 +73,15 @@ void Sandbox2D::OnImGuiRender()
 {
 	JE_PROFILE_FUNCTION();
 	ImGui::Begin("Settings");
+
+	auto stats = JetEngine::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
+
 	ImGui::ColorEdit4("SquareColor", glm::value_ptr(m_SquareColor));
 	ImGui::End();
 }
